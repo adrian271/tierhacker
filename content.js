5537a5fb23e248ce06747963fd0a090fc932bb02
Adrian Barnes <adrian@tenantinc.com>
2022-01-02 01:16:20 -0800

    💪 Add resiliency for new images to survive refresh

--- content.js
+++ content.js
@@ -1,10 +1,13 @@
 let addImg = () => {
   let img = prompt("Please enter an image url"),
-    lastImg = document.querySelector(".character:last-of-type"),
+    lastImg = document.querySelector(
+      "#create-image-carousel .character:last-of-type"
+    ),
     newItem = lastImg.cloneNode(),
-    newId = parseInt(newItem.getAttribute("id")) + 1;
+    newId = "th" + new Date().getTime().toString();
   newItem.setAttribute("id", newId);
   newItem.setAttribute("style", `background-image: url("${img}")`);
+  registerCustomImage(newId, img);
   lastImg.parentNode.append(newItem);
 };
 
@@ -14,6 +17,26 @@ let init = () => {
   btn.setAttribute("id", "tierhacker-btn");
   btn.addEventListener("click", addImg);
   document.querySelector("#create-image-carousel").after(btn);
+  restoreCustomImages();
+};
+
+let registerCustomImage = (id, imgStr) => {
+  let localData = localStorage.tierhacker || "{}";
+  let obj = JSON.parse(localData);
+  obj[id] = imgStr;
+  localStorage.setItem("tierhacker", JSON.stringify(obj));
+};
+
+let restoreCustomImages = () => {
+  let imgData = localStorage.getItem("tierhacker") || "";
+  if (imgData) {
+    let obj = JSON.parse(imgData);
+    for (item in obj) {
+      let style = document.createElement("style");
+      style.innerText = `#${item} {background-image: url(${obj[item]}) !important}`;
+      document.body.append(style);
+    }
+  }
 };
 
 document.addEventListener("DOMContentLoaded", init());
