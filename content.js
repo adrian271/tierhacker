d7f6251058c82efeebbdc1a06e65ff8014e23a0a
Adrian Barnes <adrian271@gmailcom>
2022-02-13 15:46:28 -0800

    🏞 Added Image Previewer

--- content.js
+++ content.js
@@ -1,7 +1,8 @@
 let { pathname } = window.location;
 let page = "th-" + pathname.replace(/create/gi, "").replace(/\//gi, "");
 
-let addImg = () => {
+let addImg = (ev) => {
+  ev.preventDefault();
   let input = document.querySelector("#tierhacker-input");
   let img = input.value;
   if (img) {
@@ -13,6 +14,7 @@ let addImg = () => {
     registerCustomImage(newId, img);
     document.querySelector("#create-image-carousel").append(newItem);
     input.value = "";
+    resetPreviewer();
   }
 };
 
@@ -20,16 +22,28 @@ let init = () => {
   let div = document.createElement("div");
   div.setAttribute("id", "tierhacker-container");
   document.querySelector("#create-image-carousel").after(div);
+
+  let form = document.createElement("form");
+  document.querySelector("#tierhacker-container").append(form);
+
+  let imgPreview = document.createElement("div");
+  imgPreview.setAttribute("id", "image-preview");
+  document.querySelector("#tierhacker-container").append(imgPreview);
+
   let btn = document.createElement("button");
   btn.innerText = "Add Item with Tierhacker";
   btn.setAttribute("id", "tierhacker-btn");
+  btn.disabled = true;
   btn.addEventListener("click", addImg);
+
   let input = document.createElement("input");
   input.setAttribute("id", "tierhacker-input");
   input.setAttribute("type", "text");
   input.setAttribute("placeholder", "Add image to your list");
-  document.querySelector("#tierhacker-container").append(btn);
-  document.querySelector("#tierhacker-container").prepend(input);
+  input.addEventListener("keyup", updatePreviewer);
+
+  document.querySelector("#tierhacker-container form").append(btn);
+  document.querySelector("#tierhacker-container form").prepend(input);
   restoreCustomImages();
 };
 
@@ -50,6 +64,29 @@ let restoreCustomImages = () => {
       document.body.append(style);
     }
   }
+};
+
+let updatePreviewer = () => {
+  let previewer = document.querySelector("#image-preview"),
+    btn = document.querySelector("#tierhacker-btn"),
+    newImg = document.querySelector("#tierhacker-input").value;
+  if (newImg) {
+    previewer.style.backgroundImage = `url("${newImg}")`;
+    previewer.classList.add("filled");
+    btn.disabled = false;
+  } else {
+    previewer.style.backgroundImage = ``;
+    previewer.classList.remove("filled");
+    btn.disabled = true;
+  }
+};
+
+let resetPreviewer = () => {
+  let previewer = document.querySelector("#image-preview"),
+    btn = document.querySelector("#tierhacker-btn");
+  previewer.style.backgroundImage = ``;
+  previewer.classList.remove("filled");
+  btn.disabled = true;
 };
 
 document.addEventListener("DOMContentLoaded", init());
