e7ac79a7358e877346c50040faa5880446a1cf4e
Adrian Barnes <adrian271@gmailcom>
2022-02-13 13:57:36 -0800

    Use Input instead of Prompt

--- content.js
+++ content.js
@@ -2,7 +2,8 @@ let { pathname } = window.location;
 let page = "th-" + pathname.replace(/create/gi, "").replace(/\//gi, "");
 
 let addImg = () => {
-  let img = prompt("Please enter an image url");
+  let input = document.querySelector("#tierhacker-input");
+  let img = input.value;
   if (img) {
     let lastImg = document.querySelector(".character"),
       newItem = lastImg.cloneNode(),
@@ -11,15 +12,24 @@ let addImg = () => {
     newItem.setAttribute("style", `background-image: url("${img}")`);
     registerCustomImage(newId, img);
     document.querySelector("#create-image-carousel").append(newItem);
+    input.value = "";
   }
 };
 
 let init = () => {
+  let div = document.createElement("div");
+  div.setAttribute("id", "tierhacker-container");
+  document.querySelector("#create-image-carousel").after(div);
   let btn = document.createElement("button");
   btn.innerText = "Add Item with Tierhacker";
   btn.setAttribute("id", "tierhacker-btn");
   btn.addEventListener("click", addImg);
-  document.querySelector("#create-image-carousel").after(btn);
+  let input = document.createElement("input");
+  input.setAttribute("id", "tierhacker-input");
+  input.setAttribute("type", "text");
+  input.setAttribute("placeholder", "Paste your image here");
+  document.querySelector("#tierhacker-container").append(btn);
+  document.querySelector("#tierhacker-container").prepend(input);
   restoreCustomImages();
 };
 
